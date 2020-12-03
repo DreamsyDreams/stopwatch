08183bd6f08da0e9993c5e08cd6f6767a792dacb
Valentyn Savich <vsavich_bus@outlook.com>
2020-12-03 18:58:03 -0500

    Added functions
    Added the variable to hold the interval, added a function to start, pause, and reset the stopwatch.

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const hrs = document.querySelector('.hrs');
 const mins = document.querySelector('.mins');
 const secs = document.querySelector('.secs');
 const startButton = document.querySelector('#start');
-const restartButton = document.querySelector('#restart');
+const resetButton = document.querySelector('#reset');
 const pauseButton = document.querySelector('#pause');
 
 //Defining seconds, minutes, and hours
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
+
+//Variable to hold the interval
+let interval = null;
 
 //A function to display the value
 function countTime() {
@@ -24,20 +27,65 @@ function countTime() {
     if (seconds / 60 === 1) {
         seconds = 0;
         minutes++;
-        mins.innerHTML = minutes
+        if (minutes < 10) {
+            mins.innerHTML = '0' + minutes.toString();
+        } else {
+            mins.innerHTML = minutes;
+        }
 
         //Checking if an hour passed
         if (minutes / 60 === 1) {
             minutes = 0;
             hours++;
-            hrs.innerHTML = hours
+            if (hours < 10) {
+                hrs.innerHTML = '0' + hours.toString();
+            } else {
+                hrs.innerHTML = hours;
+            }
         }
     }
 }
 
+//A function to start the stopwatch
+function start() {
+    interval = window.setInterval(countTime, 1000);
+}
+
+//A function to pause the stopwatch
+function pause() {
+    window.clearInterval(interval);
+}
+
+//A function to reset the stopwatch
+function reset() {
+    window.clearInterval(interval);
+    hrs.innerHTML = '00';
+    mins.innerHTML = '00';
+    secs.innerHTML = '00';
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+}
+
+//The start button
 startButton.addEventListener('click', button => {
     startButton.hidden = true;
     pauseButton.hidden = false;
-    restartButton.hidden = false;
-    window.setInterval(countTime, 1000);
+    resetButton.hidden = false;
+    start();
+});
+
+//The pause button
+pauseButton.addEventListener('click', button => {
+    startButton.hidden = false;
+    pauseButton.hidden = true;
+    pause();
+});
+
+//The reset button
+resetButton.addEventListener('click', button => {
+    startButton.hidden = false;
+    pauseButton.hidden = true;
+    resetButton.hidden = true;
+    reset();
 });
